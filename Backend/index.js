94b20fb2b4f67bfd0cd98bfd3ccb64e7406e0969
Neba-art <nebasebastian71@gmail.com>
2025-06-06 04:10:01 +0100

    starting working on Backend

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const dbConnection = require("./utils/db");
+dotenv.config();
+
+// port
+const PORT = process.env.PORT;
+
+
+
+// server
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`)
+    dbConnection()
+})
+
